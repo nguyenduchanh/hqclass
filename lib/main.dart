94bbf780e04cd8be95824b61c7722e66bc045484
Nguyen Duc Hanh <nguyenduchanhndh01091993@gmail.com>
2021-01-31 23:41:15 +0700

    update login form

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:hqclass/screens/classes.dart';
 import 'package:hqclass/screens/home.dart';
 import 'package:hqclass/screens/onboarding.dart';
+import 'package:provider/provider.dart';
+
+import 'Domains/user.dart';
+import 'Pages/dashboard.dart';
+import 'Pages/login.dart';
+import 'Pages/register.dart';
+import 'Pages/welcome.dart';
+import 'Providers/auth.dart';
+import 'Providers/user_provider.dart';
+import 'Util/shared_preference.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(fontFamily: 'OpenSans'),
-      initialRoute: '/home',
-      debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder> {
-        "/onboarding": (BuildContext context) => new Onboarding(),
-        "/home": (BuildContext context) => new Home(),
-        "/classes": (BuildContext context) => new Classes(),
+    Future<User> getUserData() => UserPreferences().getUser();
 
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: FutureBuilder(
+              future: getUserData(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return CircularProgressIndicator();
+                  default:
+                    if (snapshot.hasError)
+                      return Text('Error: ${snapshot.error}');
+                    else if (snapshot.data.token == null)
+                      return Login();
+                    else
+                      UserPreferences().removeUser();
+                    return Welcome(user: snapshot.data);
+                }
+              }),
+          routes: {
+            '/dashboard': (context) => DashBoard(),
+            '/login': (context) => Login(),
+            '/register': (context) => Register(),
+          }),
     );
   }
 }
