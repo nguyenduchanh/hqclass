78255cf008af3328f7aea4865f4e3d6c11c70f1d
Nguyen Duc Hanh <nguyenduchanhndh01091993@gmail.com>
2021-02-04 17:35:21 +0700

    remove debug banner

--- lib/Components/splash_screen.dart
+++ lib/Components/splash_screen.dart
@@ -14,6 +14,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SplashScreenWidget(),
     );
   }
