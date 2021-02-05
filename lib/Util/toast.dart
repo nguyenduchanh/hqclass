a3a40616357963baa55a1b50544dabb5ef456668
Nguyen Duc Hanh <nguyenduchanhndh01091993@gmail.com>
2021-02-05 19:43:17 +0700

    update toast

---
+++ lib/Util/toast.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+//
+// class ToastExample extends StatefulWidget {
+//   @override
+//   _ToastExampleState createState() {
+//     return _ToastExampleState();
+//   }
+// }
+//
+// class _ToastExampleState extends State {
+//   void showToast() {
+//     Fluttertoast.showToast(
+//         msg: 'This is toast notification',
+//         toastLength: Toast.LENGTH_SHORT,
+//         gravity: ToastGravity.BOTTOM,
+//         timeInSecForIos: 1,
+//         backgroundColor: Colors.red,
+//         textColor: Colors.yellow
+//     );
+//   }
+// }
